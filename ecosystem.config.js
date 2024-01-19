9c99a63906a64ed3b87979c4900f649ba0ff406f
dva-ren <iiuzrw@gmail.com>
2024-01-19 15:24:47 +0800

    chore: add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'satori',
+      script: 'server.js',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '180M',
+      env: {
+        PORT: 2323,
+        NODE_ENV: 'production',
+        // ...require('dotenv').config().parsed,
+      },
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
+    },
+  ],
+}
