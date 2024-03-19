5f0cad401a09df18c21e43d20ffdc28d0a50f10e
dva-ren <iiuzrw@gmail.com>
2024-03-19 15:28:44 +0800

    fix: fix header bug

--- components/common/header/index.vue
+++ components/common/header/index.vue
@@ -10,14 +10,13 @@ const masterStore = useMasterStore()
 const headerInfo = masterStore.headerInfo
 const route = useRoute()
 const isMiniSize = computed(() => useWindowSize().width.value < 768)
-
 </script>
 
 <template>
   <header
-    v-if="menus"
-    :class="showBg ? 'bg-white/80 dark:bg-black/50 dark:shadow-white/20 shadow backdrop-blur-md' : ''"
-    class="fixed left-0 right-0 top-0 z-10 h-16 w-full transition duration-200"
+  v-if="menus"
+  :class="showBg ? 'bg-white/80 dark:bg-black/50 dark:shadow-white/20 shadow backdrop-blur-md' : ''"
+  class="fixed left-0 right-0 top-0 z-10 h-16 w-full transition duration-200"
   >
     <div class="relative grid grid-cols-[4.5rem_auto_4.5rem] mx-auto h-full max-w-7xl min-h-0 lg:px-8">
       <div class="flex-center">
@@ -27,40 +26,42 @@ const isMiniSize = computed(() => useWindowSize().width.value < 768)
         </NuxtLink>
       </div>
       <div relative flex-center>
-        <Presence :exit-before-enter="false" :initial="true">
-          <CommonMotion
-            v-if="headerInfo.show && showBg"
-            class="absolute left-0 w-full flex flex-row justify-between text-omit px-2 md:px-8"
-            :initial="{ opacity: 0, y: 20 }"
-            :animate="{ opacity: 1, y: 0 }"
-            :exit="{ opacity: 0, y: 20 }"
-            :spring="{ stiffness: 400, damping: 20 }"
-          >
-            <div>
-              <div class="mb-1 text-xs text-zinc">
-                {{ headerInfo.subtitle }}
+        <div class="absolute left-0 w-full">
+          <Presence :exit-before-enter="false" :initial="true">
+            <CommonMotion
+              v-if="headerInfo.show && showBg"
+              class="flex flex-row justify-between text-omit px-2 md:px-8"
+              :initial="{ opacity: 0, y: 20 }"
+              :animate="{ opacity: 1, y: 0 }"
+              :exit="{ opacity: 0, y: 20 }"
+              :spring="{ stiffness: 400, damping: 20 }"
+            >
+              <div>
+                <div class="mb-1 text-xs text-zinc">
+                  {{ headerInfo.subtitle }}
+                </div>
+                <div class="text-omit">
+                  {{ headerInfo.title }}
+                </div>
               </div>
-              <div class="text-omit">
-                {{ headerInfo.title }}
+              <div v-if="!isMiniSize">
+                <div class="mb-1 text-xs text-zinc">
+                  {{ route.path.split('/')[-1] }}
+                </div>
+                <div class="text-x">
+                  {{ masterStore.masterInfo.name }}
+                </div>
               </div>
-            </div>
-            <div v-if="!isMiniSize">
-              <div class="mb-1 text-xs text-zinc">
-                {{ route.path.split('/')[-1] }}
-              </div>
-              <div class="text-x">
-                {{ masterStore.masterInfo.name }}
-              </div>
-            </div>
-          </CommonMotion>
-        </Presence>
-        <Transition name="fade" flex-center>
-          <NuxtLink v-if="!showBg && isMiniSize" to="/" class="- absolute left-1/2 flex-center -translate-x-1/2">
+            </CommonMotion>
+          </Presence>
+        </div>
+        <Transition name="fade">
+          <NuxtLink to="/" v-show="(!showBg || !headerInfo.show) && isMiniSize" class="absolute left-1/2 flex-center -translate-x-1/2">
             <CommonLazyLoadImage class="h-10 w-10 rounded-4 bg-gray-3" :src="masterStore.masterInfo.avatar" alt="home" />
           </NuxtLink>
         </Transition>
         <Transition name="fade" flex-center>
-          <CommonHeaderNav v-if="!showBg || !headerInfo.show" :menus="menus.data" class="hidden lg:flex" />
+          <CommonHeaderNav v-if="(!showBg || !headerInfo.show) && !isMiniSize" :menus="menus.data" class="hidden lg:flex" />
         </Transition>
       </div>
       <div class="flex-center">
